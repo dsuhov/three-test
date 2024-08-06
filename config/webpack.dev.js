977cf42351d1e3564bb61b1f8721f711aa1edd39
d.sukhov <d.sukhov@sbereducation.ru>
2024-08-06 18:03:43 +0300

    add particles

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,11 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
+const { merge } = require('webpack-merge');
+const commonConfiguration = require('./webpack.common.js');
 
-module.exports = merge(
-  commonConfiguration,
-  {
-    mode: 'development',
-    devServer: {
-      open: true,
-      port: 3000,
-    }
-  }
-);
+module.exports = merge(commonConfiguration, {
+  mode: 'development',
+  devServer: {
+    open: true,
+    port: 3000,
+    static: ['static'],
+  },
+});
--- src/script.js
+++ src/script.js
@@ -19,14 +19,51 @@ window.addEventListener('resize', () => {
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 50000;
+
+const positions = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+}
+
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3),
+);
+
+const particlesMaterial = new THREE.PointsMaterial();
+
+particlesMaterial.size = 0.02;
+particlesMaterial.sizeAttenuation = true;
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+// const geometry = new THREE.TorusKnotGeometry(1.2, 0.4, 160, 20);
+// const material = new THREE.MeshBasicMaterial({
+//   color: 0xff0000,
+//   wireframe: true,
+// });
+// geometry.parameters.radialSegments = 20;
+// geometry.parameters.tubularSegments = 160;
+
+// const material = new THREE.MeshStandardMaterial();
+// material.wireframe = true;
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// const light = new THREE.PointLight(0xffffff, 10);
+// light.position.set(2, 3, 4);
+// scene.add(light);
+
+// const light2 = new THREE.AmbientLight(0xffffff, 0.02);
+// light2.position.set(-2, -3, -4);
+// scene.add(light2);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(2, 2, 2);
+camera.position.set(10, 8, 10);
 // scene.add(camera);
 
 const axesHelper = new THREE.AxesHelper(2);
