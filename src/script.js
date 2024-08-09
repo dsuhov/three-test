8dd085441e54529f1482a7c12ca89cd38ca00747
d.sukhov <d.sukhov@sbereducation.ru>
2024-08-09 17:01:36 +0300

    particles video

--- src/script.js
+++ src/script.js
@@ -1,48 +1,44 @@
 import './style.css';
 
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
 import { scene, run } from './setup';
-import { TeapotGeometry } from 'three/addons/geometries/TeapotGeometry.js';
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js';
 
-const planeGeometry = new THREE.PlaneGeometry(3, 3);
-const material = new THREE.MeshStandardMaterial({
-  color: 0xffff00,
+const gui = new dat.GUI();
+
+const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load('/2.png');
+// console.log(particleTexture);
+
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 6000;
+
+const positions = new Float32Array(count * 3);
+for (let i = 0; i < count; i++) {
+  positions[i] = (Math.random() - 0.5) * 3;
+}
+
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3),
+);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
 });
-const plane = new THREE.Mesh(planeGeometry, material);
-plane.rotateX(Math.PI * -0.5);
-scene.add(plane);
+particlesMaterial.color = new THREE.Color('#bada55');
+// particlesMaterial.map = particleTexture;
+particlesMaterial.alphaMap = particleTexture;
+particlesMaterial.transparent = true;
+// 1 way
+// particlesMaterial.alphaTest = 0.01;
+// 2 way
+particlesMaterial.depthTest = false;
 
-const teapotMaterialConfig = {
-  clearcoat: 0.1,
-  clearcoatRoughness: 0.15,
-  metalness: 0.3,
-  roughness: 0.5,
-  color: '#a739ec',
-};
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 
-const teapotGeometry = new TeapotGeometry(0.5, 18);
-// test material
-const teapotMaterial = new THREE.MeshPhysicalMaterial(teapotMaterialConfig);
-const teapot = new THREE.Mesh(teapotGeometry, teapotMaterial);
-teapot.position.y = 0.5;
-scene.add(teapot);
+scene.add(particles);
 
-const light = new THREE.PointLight(0xffffff, 2, 100);
-light.position.set(1, 2, 1);
-scene.add(light);
-
-const pointLightHelper = new THREE.PointLightHelper(light, 0.2);
-scene.add(pointLightHelper);
-
-const rectLight = new THREE.RectAreaLight(0xffffff, 3, 1, 1);
-rectLight.position.set(0, 2, -2);
-
-rectLight.rotateY(Math.PI);
-rectLight.rotateX(-0.71);
-scene.add(rectLight);
-
-const rectLightHelper = new RectAreaLightHelper(rectLight);
-rectLight.add(rectLightHelper);
-
-run();
+run(particles);
